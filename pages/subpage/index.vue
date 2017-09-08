6aa20d959f805a4819041f1cb45293fb9d9531a5
garr1s0n <gary.wilpizeski@gmail.com>
2017-09-08 12:34:51 -0400

    Initial Heroku Build

--- pages/subpage/index.vue
+++ pages/subpage/index.vue
@@ -1,29 +1,22 @@
 <template>
   <section class="container">
     <div>
-      <logo/>
       <h1 class="title">
-        portfolio
+        Subpage
       </h1>
       <h2 class="subtitle">
-        Front End Developer portfolio SPA for Gary Wilpizeski
+        This is a subpage.
       </h2>
       <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green">Documentation</a>
-        <a href="/subpage" class="button--grey">Subpage Here</a>
-        <a href="https://github.com/nuxt/nuxt.js" target="_blank" class="button--grey">GitHub</a>
+        <a href="/" class="button--green">Home</a>
       </div>
     </div>
   </section>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
 
 export default {
-  components: {
-    Logo
-  }
 }
 </script>
 
