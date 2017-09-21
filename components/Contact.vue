7dac7a40cef9bf095ef73afd47eabe9dc4a407b3
garr1s0n <gary.wilpizeski@gmail.com>
2017-09-21 13:50:09 -0400

    init contact component

--- components/Contact.vue
+++ components/Contact.vue
@@ -1,7 +1,12 @@
 <template>
     <div class="container">
       <div>
-
+          <div class="row">
+            <div class="headerText column twelve">
+                <h1>send me a message!</h1>
+                <p>Interested in doing business? Want to learn more? Just want to say hi? Fill out the form below and drop me a line!</p>
+            </div>
+          </div>
       </div>
     </div>
 </template>
@@ -14,6 +19,12 @@ export default {
 
 <<style lang="less">
 @import url('../static/styles/base-styles.less');
+
+h1 {
+    font-family: @playfair;
+    font-size: 1.35em;
+    color @white;
+}
 
 </style>
 
